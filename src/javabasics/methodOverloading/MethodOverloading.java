7bee7aafdf92bfda646c6c2802b950d3b7587323
mobin-newroz <ahmobin@newroztech.com>
2022-02-13 16:31:43 +0600

    area calculator challenge

--- src/javabasics/methodOverloading/MethodOverloading.java
+++ src/javabasics/methodOverloading/MethodOverloading.java
@@ -3,8 +3,9 @@ package javabasics.methodOverloading;
 public class MethodOverloading {
     public static void main(String[] args){
         System.out.println("Method Overloading: Method overloading means: the same name of multiple method with same or different types & number of parameters");
-
         System.out.println(OverloadingChallenge.getDurationString(65,45));
         System.out.println(OverloadingChallenge.getDurationString(3945));
+
+        System.out.println(AreaCalculator.area(-1.0,4.0));
     }
 }
