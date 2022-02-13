ce07170c0d01ae06c3b0388be0701f6f6d6354ad
mobin-newroz <ahmobin@newroztech.com>
2022-02-13 16:20:47 +0600

    update second minutes challenge

--- src/javabasics/methodOverloading/OverloadingChallenge.java
+++ src/javabasics/methodOverloading/OverloadingChallenge.java
@@ -1,18 +1,32 @@
 package javabasics.methodOverloading;
 
 public class OverloadingChallenge {
+    private static final String INVALID_VALUE_MESSAGE = "Invalid Value";
     public static String getDurationString(int minutes, int seconds){
         if(minutes < 0 && (seconds < 0 || seconds > 59))
-            return "invalid value";
+            return INVALID_VALUE_MESSAGE;
 
         int hours = minutes / 60;
         minutes = minutes % 60;
-        return hours + "h " + minutes +"m " + seconds + "s";
+
+        String hourString = hours + "h";
+        if(hours < 10)
+            hourString = "0"+hourString;
+
+        String minuteString = minutes + "m";
+        if(minutes < 10)
+            minuteString = "0"+minuteString;
+
+        String secondString = seconds + "s";
+        if(seconds < 10)
+            secondString = "0"+secondString;
+
+        return hourString + " " + minuteString +" " + secondString + " ";
     }
 
     public static String getDurationString(int seconds){
         if(seconds < 0 )
-            return "invalid value";
+            return INVALID_VALUE_MESSAGE;
 
         int minutes = seconds / 60;
         seconds = seconds % 60;
