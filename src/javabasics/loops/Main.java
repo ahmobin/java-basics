c25d97a821c5f755d16111ad742e09dde8ad3237
mobin-newroz <ahmobin@newroztech.com>
2022-02-15 16:56:37 +0600

    palindrome number check

--- src/javabasics/loops/Main.java
+++ src/javabasics/loops/Main.java
@@ -2,15 +2,18 @@ package javabasics.loops;
 
 public class Main {
     public static void main(String[] args) {
-        SumThreeFiveChallenge.Sum();
-        System.out.println("======================================");
+//        SumThreeFiveChallenge.Sum();
+//        System.out.println("======================================");
         //sum of odds on range challenge ---
-//        System.out.println(SumOddChallenge.isOdd(-1));
-        System.out.println(SumOddChallenge.sumOdd(1,100));
-        System.out.println(SumOddChallenge.sumOdd(-1,100));
-        System.out.println(SumOddChallenge.sumOdd(100,100));
-        System.out.println(SumOddChallenge.sumOdd(13,13));
-        System.out.println(SumOddChallenge.sumOdd(100,-100));
-        System.out.println(SumOddChallenge.sumOdd(100,1000));
+//        System.out.println(SumOddChallenge.sumOdd(1,100));
+//        System.out.println(SumOddChallenge.sumOdd(-1,100));
+//        System.out.println(SumOddChallenge.sumOdd(100,100));
+//        System.out.println(SumOddChallenge.sumOdd(13,13));
+//        System.out.println(SumOddChallenge.sumOdd(100,-100));
+//        System.out.println(SumOddChallenge.sumOdd(100,1000));
+//        System.out.println("======================================");
+
+        //palindrome number check
+        System.out.println(NumberPalindrome.isPalindrome(181));
     }
 }
