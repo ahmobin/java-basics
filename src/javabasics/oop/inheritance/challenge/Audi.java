ee0651b14148771fcc1ccfd72f58e851f44bcbeb
mobin-newroz <ahmobin@newroztech.com>
2022-02-20 14:39:49 +0600

    inheritance first challenge

---
+++ src/javabasics/oop/inheritance/challenge/Audi.java
@@ -0,0 +1,30 @@
+package javabasics.oop.inheritance.challenge;
+
+public class Audi extends Car{
+    private int roadServiceMonths;
+
+    public Audi(int roadServiceMonths) {
+        super("Audi", "4WD", 4, 4, 6, true);
+        this.roadServiceMonths = roadServiceMonths;
+    }
+
+    public void accelerate(int rate){
+        int newVelocity = getCurrentVelocity() + rate;
+        if(newVelocity == 0){
+            stop();
+            changeGear(1);
+        }else if( newVelocity > 0 && newVelocity <=10){
+            changeGear(1);
+        }else if( newVelocity > 0 && newVelocity <=20){
+            changeGear(2);
+        }else if (newVelocity > 10 && newVelocity <= 30){
+            changeGear(3);
+        }else{
+            changeGear(4);
+        }
+
+        if(newVelocity > 0){
+            changeVelocity(newVelocity, getCurrentDirection());
+        }
+    }
+}
---
+++ src/javabasics/oop/inheritance/challenge/Main.java
@@ -0,0 +1,9 @@
+package javabasics.oop.inheritance.challenge;
+
+public class Main {
+    public static void main(String[] args){
+        Audi audi = new Audi(40);
+        audi.steer(30);
+        audi.accelerate(30);
+    }
+}
