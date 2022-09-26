31ff0092750f5e9a6d4c55fab6f313a1ea90c6a0
mobin-newroz <ahmobin@newroztech.com>
2022-09-26 19:45:59 +0600

    fix roman to integer convert

--- src/leetcode/RomanToIntegerSolution.java
+++ src/leetcode/RomanToIntegerSolution.java
@@ -6,26 +6,36 @@ public class RomanToIntegerSolution {
 
     public static void main(String[] args) {
         RomanToIntegerSolution romanToIntegerSolution = new RomanToIntegerSolution();
-        romanToIntegerSolution.romanToInt("III");
+        System.out.println(romanToIntegerSolution.romanToInt("MCMXCIV"));
     }
 
-    public int romanToInt(String romanValue) {
-        HashMap<String, Integer> romanInteger = new HashMap<>();
-        romanInteger.put("I",1);
-        romanInteger.put("V",5);
-        romanInteger.put("X",10);
-        romanInteger.put("L",50);
-        romanInteger.put("C",100);
-        romanInteger.put("D",500);
-        romanInteger.put("M",1000);
+    public int romanToInt(String s) {
+        HashMap<Character, Integer> romanInteger = new HashMap<>();
+        romanInteger.put('I',1);
+        romanInteger.put('V',5);
+        romanInteger.put('X',10);
+        romanInteger.put('L',50);
+        romanInteger.put('C',100);
+        romanInteger.put('D',500);
+        romanInteger.put('M',1000);
 
-        int sum = 0;
+        int total = 0;
+        for (int i = 0; i<s.length(); i++){
+            int romanIntValue = romanInteger.get(s.charAt(i));
 
-        for(int i=0; i<romanValue.length(); i++){
-            System.out.print(romanValue.charAt(i)+"\n");
-            sum += romanInteger.get("I");
+            if(i>0){
+                total += romanIntValue;
+                if(romanInteger.get(s.charAt(i)) > romanInteger.get(s.charAt(i-1))){
+                    total -= romanIntValue + romanInteger.get(s.charAt(i-1));
+                    romanIntValue = romanInteger.get(s.charAt(i)) - romanInteger.get(s.charAt(i-1));
+                    total += romanIntValue;
+                }
+            }else{
+                total = romanIntValue;
+            }
         }
 
-        return sum;
+
+        return total;
     }
 }
